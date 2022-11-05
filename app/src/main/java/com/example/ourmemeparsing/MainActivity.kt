ff1e78a26dc75991df2a2b956e558b01ad32fe97
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-11-05 15:00:54 +0300

    Some examples of better approach of working with Jsoup or background thread and error handling

--- app/src/main/java/com/example/ourmemeparsing/MainActivity.kt
+++ app/src/main/java/com/example/ourmemeparsing/MainActivity.kt
@@ -1,23 +1,71 @@
 package com.example.ourmemeparsing
 
-import android.R
+import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
 import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.ourmemeparsing.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 import java.io.IOException
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val start_btn: Button = findViewById(R.id.button1)
+//        start_btn.setOnClickListener(View.OnClickListener {
+//            try {
+//                parse.parsing()
+//            } catch (e: IOException) {
+//                e.printStackTrace()
+//            }
+//        })
+//    }
+
+    private val binding : ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val start_btn: Button = findViewById(R.id.button1)
-        start_btn.setOnClickListener(View.OnClickListener {
-            try {
-                parse.parsing()
-            } catch (e: IOException) {
-                e.printStackTrace()
+        setContentView(binding.root)
+        configureViews()
+    }
+
+    private fun configureViews() {
+        with(binding) {
+            button1.setOnClickListener(onClick2)
+        }
+    }
+
+    private val onCLick1 = OnClickListener {
+        lifecycleScope.launch {
+            parse.idealWayWithWorkNotOnMainThread()
+        }
+    }
+
+    /** Еще вариант без корутин стандартными средствами Java и Android **/
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val onClick2 = OnClickListener {
+        thread(start = true) {
+            parse.parsingWithNoNullable2()
+            /** По ТЗ это не нужно, просто демонстрация,
+             * как быть если из нового потока нужно было бы данные отправить
+             * на главный поток (допустим, для отрисовки UI (через handler)
+             */
+            handler.post {
+                Toast.makeText(this, "Logged the data successfully", Toast.LENGTH_SHORT).show()
             }
-        })
+        }
     }
 }
--- app/src/main/java/com/example/ourmemeparsing/parse.kt
+++ app/src/main/java/com/example/ourmemeparsing/parse.kt
@@ -1,16 +1,27 @@
 package com.example.ourmemeparsing
 
+import android.os.NetworkOnMainThreadException
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import java.io.IOException
 
+
+/** Зачем object, почему object, за что object???
+ * Кажется, простой класс (ParseService) тут подошел бы лучше. Хотя если по задумке тут Singleton
+ * просто нужен, то и object пойдет, наверное
+ */
 object parse {
+
+    /** Есть куча вопросов к этому методу, ниже идеи что и как можно исправить **/
     @Throws(IOException::class)
     fun parsing() {
-        val policy = ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
+//        val policy = ThreadPolicy.Builder().permitAll().build()
+//        StrictMode.setThreadPolicy(policy)
         var doc: Document? = null
         try {
             doc = Jsoup.connect("https://vk.com/memzavod1523l").maxBodySize(0).get()
@@ -23,4 +34,55 @@ object parse {
         }
     }
 
+    /** Kotlin-way **/
+    fun parsingWithNoNullable1() {
+        runCatching {
+            val doc = Jsoup.connect("https://vk.com/memzavod1523l").maxBodySize(0).get().apply {
+                body().select(".wall_text").forEach { element ->
+                    Log.d("ELEMENT", element.text())
+                }
+            }
+        }.onFailure {
+            Log.d("EXCEPTION", it.stackTrace.toString())
+        }
+    }
+
+    fun parsingWithNoNullable2() {
+        val news = try {
+            Jsoup.connect("https://vk.com/memzavod1523l").maxBodySize(0).get()
+                .body().select(".wall_text").forEach { element ->
+                    Log.d("ELEMENT", element.text())
+                }
+        } catch (exception: NetworkOnMainThreadException) {
+            Log.d("EXCEPTION", exception.message.toString())
+        }
+    }
+
+    /** Теперь вернемся к тому, почему же вообще мы ловим тут network on main thread
+     * и зачем нужны страшные строчки (а они вообще не нужны и очень даже вредны по множеству причин
+     * val policy = ThreadPolicy.Builder().permitAll().build()
+     * StrictMode.setThreadPolicy(policy)
+     */
+
+    suspend fun idealWayWithWorkNotOnMainThread() {
+        val doc = withContext(Dispatchers.IO) {
+            Jsoup.connect("https://vk.com/memzavod1523l").maxBodySize(0).get()
+        }.apply {
+            body().select(".wall_text").forEach { element ->
+                Log.d("ELEMENT", element.text())
+            }
+        }
+        /** Не изучал либу, если нужно чистить ресурсы, то просто у doc потом вызовем нужный метод
+         * Если не нужно, ниже метод, который не создает лишнюю переменную
+         */
+    }
+
+    suspend fun idealWayWithWorkNotOnMainThreadVariant2() {
+        withContext(Dispatchers.IO) {
+            Jsoup.connect("https://vk.com/memzavod1523l").maxBodySize(0).get()
+                .body().select(".wall_text").forEach { element ->
+                Log.d("ELEMENT", element.text())
+            }
+        }
+    }
 }
